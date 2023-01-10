f838bb57f2e4fc15a6ac1ecefe510ecaccbb1414
pattharajarin <pattharajarin_p@cmu.ac.th>
2023-01-10 15:21:41 +0700

    ok

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -1,9 +1,19 @@
 #include<iostream>
 using namespace std;
 
+double gcd(int x , int y);
+
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+
+double gcd(int x , int y){
+    if(x%y==0){
+        return y ;
+    }else{
+        return gcd(y, x%y) ;
+    }
 }
